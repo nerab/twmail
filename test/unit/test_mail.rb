0cba654c1b7490f5ef1130d390ee1c452b930dab
Nicholas E. Rabenau <nerab@gmx.at>
2014-06-20 18:34:44 +0200

    Add tests for mails with Usenet-compatible signature

---
+++ test/unit/test_mail.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+require 'mail'
+
+class TestMail < Test::Unit::TestCase
+  def test_signature
+    m = Mail.new(File.read(fixture('mail_with_signature.txt')))
+    assert(m)
+    assert(m.body.include?('-- '))
+    assert_false(m.body.decoded.split('-- ').include?('-- '))
+  end
+
+  def fixture(name)
+    File.join(File.dirname(__FILE__), '..', 'fixtures', name)
+  end
+end
